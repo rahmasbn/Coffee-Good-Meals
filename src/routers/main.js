75d832d663e1a3775bd817ee28eb9d76c2403197
special-snowflake <fzn475@gmail.com>
2022-01-26 22:33:57 +0700

    adding routers

--- src/routers/main.js
+++ src/routers/main.js
@@ -1,6 +1,19 @@
-const express = require("express");
+const {Router} = require('express');
+const express = require('express');
 const mainRouter = express.Router();
 
+const authRouter = require('./auth');
+const categoryRouter = require('./category');
+const productsRouter = require('./products');
+const promosRouter = require('./promos');
+const transactionRouter = require('./transactions');
+const userRouter = require('./users');
 
+mainRouter.use('/auth', authRouter);
+mainRouter.use('/category', categoryRouter);
+mainRouter.use('/products', productsRouter);
+mainRouter.use('/promos', promosRouter);
+mainRouter.use('/transaction', transactionRouter);
+mainRouter.use('/users', userRouter);
 
 module.exports = mainRouter;
--- src/routers/products.js
+++ src/routers/products.js
@@ -1,1 +1,4 @@
-console.log('products');
+const express = require('express');
+const productsRouter = express.Router();
+
+module.exports = productsRouter;
