a9e8f137740a0382bd389f57f8474de87afe2316
Rahmasbn <rahmasyabani37@gmail.com>
2022-01-29 15:22:01 +0700

    add logout and fix response

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,13 +3,14 @@ const express = require("express");
 const authRouter = express.Router();
 const authController = require("../controllers/auth");
 const validate = require("../middlewares/validate");
+const authorize = require("../middlewares/authorize");
 
 // /auth
 authRouter.post("/register", validate.register, authController.register);
 authRouter.post("/login", validate.login, authController.login);
-authRouter.post("/forgot-password", authController.forgotPassword)
-authRouter.post("/checkOTP", authController.checkOTP)
-authRouter.post("/reset-password", authController.resetPassword)
-
+authRouter.post("/forgot-password", authController.forgotPassword);
+authRouter.post("/checkOTP", authController.checkOTP);
+authRouter.post("/reset-password", authController.resetPassword);
+authRouter.delete("/", authorize.checkToken, authController.logout);
 
 module.exports = authRouter;
