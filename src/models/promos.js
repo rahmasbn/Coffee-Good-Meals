e8fea122cc0bd176694c7b9b90a42d047d3ab5bd
special-snowflake <fzn475@gmail.com>
2022-03-22 16:42:39 +0700

    fix promo

--- src/models/promos.js
+++ src/models/promos.js
@@ -37,7 +37,7 @@ const addPromos = (body) => {
 const getListPromo = () => {
   return new Promise((resolve, reject) => {
     const today = getToday();
-    const sqlGetList = `SELECT id, name, description, code, discount, image, R, X, XL, dine_in, take_away, home_delivery FROM promo
+    const sqlGetList = `SELECT id, id_category, name, description, code, discount, image, R, X, XL, dine_in, take_away, home_delivery FROM promo
       WHERE discount_start < ? AND discount_end > ? AND deleted_at IS NULL
       ORDER BY created_at DESC`;
     db.query(sqlGetList, [today, today], (err, result) => {
@@ -56,7 +56,7 @@ const getListPromo = () => {
 
 const getPromoDetail = (id) => {
   return new Promise((resolve, reject) => {
-    const sqlGetList = `SELECT id, name, description, code, 
+    const sqlGetList = `SELECT id, id_category, name, description, code, 
     discount, discount_start, discount_end, image, R, X, XL, dine_in, take_away, home_delivery
     FROM promo
     WHERE id = ? AND deleted_at IS NULL
